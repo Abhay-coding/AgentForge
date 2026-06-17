78a240babab680a324d66bb8e6ac57b1a9f3a4df
Abhay Kumar <abhay.kumar.8669@gmail.com>
2026-06-17 11:35:00 +0530

    multi-agent AI system with memory and YouTube analyzer

---
+++ app/agents/team.py
@@ -0,0 +1,24 @@
+from agno.agent import Agent
+from agno.models.groq import Groq
+from dotenv import load_dotenv
+from agno.team import Team
+
+load_dotenv()
+
+eng_agent = Agent(name="English Agent", role="You answer questions in English")
+chi_agent = Agent(name="Chinese Agent", role="You answer questions in Chinese")
+hin_agent = Agent(name="Hindi Agent", role="You answer questions in Hindi")
+
+team_leader = Team (
+    name = "Answer & Translation Team",
+    members = [eng_agent,chi_agent,hin_agent],
+    model=Groq(id="qwen/qwen3-32b"),
+    markdown =True,
+    # show_members_responses = True,
+    instructions = """All member agents must respond to answer the query in their specific language.
+                        Do not route to just one agent
+                        Output the response of all agent                         
+            """
+)
+
+team_leader.print_response("What is the Capital of India?",stream=True,)
